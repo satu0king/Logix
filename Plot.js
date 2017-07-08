98ac2034e038db1d3829cb6f2a6b0277fd85b2e8
Satvik Ramaprasad <satviksunkam.ramaprasad@iiitb.org>
2017-07-08 14:08:08 +0530

    renaming cox

--- Plot.js
+++ Plot.js
@@ -37,6 +37,7 @@ var plotArea = {
   oy : 0,
   unit : 0,
   c : document.getElementById("plotArea"),
+
   count:0,
   specificTimeX:0,
   scale : 1,
@@ -47,6 +48,8 @@ var plotArea = {
   setup:function(){
       this.stopWatch =new StopWatch()
       this.stopWatch.Start();
+      this.context=this.c.getContext("2d");
+    //   console.log(this.context);
       startPlot();
       this.timeOutPlot = setInterval(function(){
         plotArea.plot();
@@ -67,20 +70,21 @@ var plotArea = {
       else if(!plotArea.mouseDown){
           this.ox-=plotArea.scrollAcc;
           plotArea.scrollAcc*=0.95;
-          if(this.ox<0)plotArea.scrollAcc=-0.5+0.2*this.ox;
+          if(this.ox<0)plotArea.scrollAcc=-0.2+0.2*this.ox;
+          if(Math.abs(this.ox)<0.5)this.ox=0;
       }
-      context = this.c.getContext("2d");
-      this.clear();
-      context.fillStyle = 'black';
-      context.fillRect(0, 0, this.c.width, this.c.height);
+      var ctx = this.context;
+      this.clear(ctx);
+      ctx.fillStyle = 'black';
+      ctx.fillRect(0, 0, this.c.width, this.c.height);
       var unit= (this.pixel/(plotArea.unit*plotArea.scale))
-      context.strokeStyle = 'cyan';
-      context.lineWidth = 2;
+      ctx.strokeStyle = 'cyan';
+      ctx.lineWidth = 2;
       for(var i=0;i<globalScope.Output.length;i++)
       {
 
         var arr=globalScope.Output[i].plotValue;
-        // context.moveTo(80-plotArea.ox,2*(30+i*15-5*arr[0][1])-plotArea.oy);
+        // ctx.moveTo(80-plotArea.ox,2*(30+i*15-5*arr[0][1])-plotArea.oy);
         // if(plotArea.count==i){
         //   arr.push([time,arr[arr.length-1][1]]);
         //   plotArea.count+=1;
@@ -101,159 +105,104 @@ var plotArea = {
 
 
 
-              if(globalScope.Output[i].plotValue[j][1]==1 && globalScope.Output[i].bitWidth==1)
+              if(globalScope.Output[i].bitWidth==1)
               {
-                context.lineTo(80+(start*unit)-plotArea.ox,2*(25+i*15)-plotArea.oy);
-                context.lineTo(80+(end*unit)-plotArea.ox,2*(25+i*15)-plotArea.oy);
-                // context.lineTo(80+(end*unit)-plotArea.ox,2*(30+i*15)-plotArea.oy);
-
-                // context.stroke();
-              }
-              else if(globalScope.Output[i].bitWidth==1) {
-                context.lineTo(80+(start*unit)-plotArea.ox,2*(30+i*15)-plotArea.oy);
-                context.lineTo(80+(end*unit)-plotArea.ox,2*(30+i*15)-plotArea.oy);
-                // context.stroke();
+                ctx.lineTo(80+(start*unit)-plotArea.ox,2*(30+i*15-5*arr[j][1])-plotArea.oy);
+                ctx.lineTo(80+(end*unit)-plotArea.ox,2*(30+i*15-5*arr[j][1])-plotArea.oy);
               }
               else {
-                context.beginPath();
-                context.moveTo(80+(end*unit)-plotArea.ox,55+30*i-plotArea.oy);
-                context.lineTo(80+(end*unit)-5-plotArea.ox,2*(25+i*15)-plotArea.oy);
-                context.lineTo(80+(start*unit)+5-plotArea.ox,2*(25+i*15)-plotArea.oy);
-                context.lineTo(80+(start*unit)-plotArea.ox,55+30*i-plotArea.oy);
-                context.lineTo(80+(start*unit)+5-plotArea.ox,2*(30+i*15)-plotArea.oy);
-                context.lineTo(80+(end*unit)-5-plotArea.ox,2*(30+i*15)-plotArea.oy);
-                context.lineTo(80+(end*unit)-plotArea.ox,55+30*i-plotArea.oy);
+                ctx.beginPath();
+                ctx.moveTo(80+(end*unit)-plotArea.ox,55+30*i-plotArea.oy);
+                ctx.lineTo(80+(end*unit)-5-plotArea.ox,2*(25+i*15)-plotArea.oy);
+                ctx.lineTo(80+(start*unit)+5-plotArea.ox,2*(25+i*15)-plotArea.oy);
+                ctx.lineTo(80+(start*unit)-plotArea.ox,55+30*i-plotArea.oy);
+                ctx.lineTo(80+(start*unit)+5-plotArea.ox,2*(30+i*15)-plotArea.oy);
+                ctx.lineTo(80+(end*unit)-5-plotArea.ox,2*(30+i*15)-plotArea.oy);
+                ctx.lineTo(80+(end*unit)-plotArea.ox,55+30*i-plotArea.oy);
                 mid = 80+((end+start)/Math.round(plotArea.unit*plotArea.scale))*this.pixel/2;
-                context.font="12px Georgia";
-                context.fillStyle = 'white';
+                ctx.font="12px Georgia";
+                ctx.fillStyle = 'white';
                 if((start*unit)+10-plotArea.ox <=0 && (end*unit)+10-plotArea.ox >=0){
                     mid = 80+((end-3000)/Math.round(plotArea.unit*plotArea.scale))*this.pixel;
                   }
-                context.fillText(arr[j][1],mid-plotArea.ox,57+30*i-plotArea.oy);
-                context.stroke();
+
+                ctx.fillText(arr[j][1],mid-plotArea.ox,57+30*i-plotArea.oy);
+                ctx.stroke();
               }
             }
             else {
               break;
             }
         }
-        context.stroke();
-        context.beginPath();
+        ctx.stroke();
+        ctx.beginPath();
       }
       // 2 rectangles showing the time and labels
 
-      context.fillStyle = '#eee';
-      context.fillRect(0, 0, this.c.width, 30);
-      context.font="14px Georgia";
-      context.fillStyle = 'black';
+      ctx.fillStyle = '#eee';
+      ctx.fillRect(0, 0, this.c.width, 30);
+      ctx.font="14px Georgia";
+      ctx.fillStyle = 'black';
       for(var i=1; i*Math.round(plotArea.unit*plotArea.scale)<=time + Math.round(plotArea.unit*plotArea.scale) ;i++)
       {
-        context.fillText(Math.round(plotArea.unit*plotArea.scale)*i/1000+"s",48+this.pixel*i-plotArea.ox,20);
+        ctx.fillText(Math.round(plotArea.unit*plotArea.scale)*i/1000+"s",48+this.pixel*i-plotArea.ox,20);
 
       }
 
-      context.fillStyle = '#eee';
-      context.fillRect(0,0,75,this.c.height);
-      context.font="15px Georgia";
-      context.fillStyle = 'black';
+      ctx.fillStyle = '#eee';
+      ctx.fillRect(0,0,75,this.c.height);
+      ctx.font="15px Georgia";
+      ctx.fillStyle = 'black';
       for(var i=0;i<globalScope.Output.length;i++){
-        context.fillText(globalScope.Output[i].label,5,2*(25+i*15));
-        context.fillRect(0,2*(13+i*15)+4 , 75, 3);
+        ctx.fillText(globalScope.Output[i].label,5,2*(25+i*15));
+        ctx.fillRect(0,2*(13+i*15)+4 , 75, 3);
       }
-      context.font="16px Georgia";
-      context.fillText("Time",10,20);
+      ctx.font="16px Georgia";
+      ctx.fillText("Time",10,20);
 
       // for yellow line to show specific time
       var specificTime = (plotArea.specificTimeX+plotArea.ox-80)*Math.round(plotArea.unit*plotArea.scale)/(this.pixel);;
-      context.strokeStyle = 'white';
-      context.moveTo(plotArea.specificTimeX,0);
-      context.lineTo(plotArea.specificTimeX,plotArea.c.height);
-      context.stroke();
+      ctx.strokeStyle = 'white';
+      ctx.moveTo(plotArea.specificTimeX,0);
+      ctx.lineTo(plotArea.specificTimeX,plotArea.c.height);
+      ctx.stroke();
       if(1.115*plotArea.specificTimeX >= 80){
-        context.fillStyle = 'black';
-        context.fillRect(plotArea.specificTimeX - 35, 0, 70, 30);
-        context.fillStyle = 'red';
-        context.fillRect(plotArea.specificTimeX - 30, 2, 60, 26);
-        context.font="20px Georgia";
-        context.fillStyle = 'black';
-        context.fillText(Math.round(specificTime),plotArea.specificTimeX - 28, 20);
+        ctx.fillStyle = 'black';
+        ctx.fillRect(plotArea.specificTimeX - 35, 0, 70, 30);
+        ctx.fillStyle = 'red';
+        ctx.fillRect(plotArea.specificTimeX - 30, 2, 60, 26);
+        ctx.font="20px Georgia";
+        ctx.fillStyle = 'black';
+        ctx.fillText(Math.round(specificTime),plotArea.specificTimeX - 28, 20);
       }
       // borders
-      context.strokeStyle = 'black';
-      context.lineWidth = 2;
-      context.beginPath();
-      context.moveTo(0,0);
-      context.lineTo(0,this.c.height);
-    //   context.fillRect(0, 0, 3, this.c.height);
+      ctx.strokeStyle = 'black';
+      ctx.lineWidth = 2;
+      ctx.beginPath();
+      ctx.moveTo(0,0);
+      ctx.lineTo(0,this.c.height);
+    //   ctx.fillRect(0, 0, 3, this.c.height);
 
-    context.moveTo(74,0);
-    context.lineTo(74,this.c.height);
-    //   context.fillRect(74, 0, 3, this.c.height);
+    ctx.moveTo(74,0);
+    ctx.lineTo(74,this.c.height);
+    //   ctx.fillRect(74, 0, 3, this.c.height);
 
-    context.moveTo(0,0);
-    context.lineTo(this.c.width,0);
-    //   context.fillRect(0, 0, this.c.width, 3);
+    ctx.moveTo(0,0);
+    ctx.lineTo(this.c.width,0);
+    //   ctx.fillRect(0, 0, this.c.width, 3);
 
-    // context.moveTo(0,27);
-    // context.lineTo(this.c.width,27);
-    //   context.fillRect(0, 27, this.c.width, 3);
-      context.stroke();
+    // ctx.moveTo(0,27);
+    // ctx.lineTo(this.c.width,27);
+    //   ctx.fillRect(0, 27, this.c.width, 3);
+      ctx.stroke();
   },
-  clear: function(){
-    context.clearRect(0,0,plotArea.c.width,plotArea.c.height);
+  clear: function(ctx){
+    ctx.clearRect(0,0,plotArea.c.width,plotArea.c.height);
     // clearInterval(timeOutPlot);
   }
 
 }
 
-// listen
-  // window.addEventListener('keydown', function(e) {
-  //   if (e.keyCode == 37){
-  //     if(plotArea.ox >= this.unit){
-  //       plotArea.ox -= this.unit;
-  //     }
-  //     else{
-  //       plotArea.ox = 0;
-  //     }
-  //   }
-  //   if(e.keyCode == 39){
-  //         plotArea.ox += this.unit;
-  //     }
-  //     if (e.keyCode == 38){
-  //       if(plotArea.oy >= 15){
-  //       plotArea.oy -= 15;
-  //     }
-  //     else{
-  //       plotArea.oy = 0;
-  //     }
-  //   }
-  //   if (e.keyCode == 40){
-  //     plotArea.oy += 15;
-  //   }
-  //   if (e.keyCode == 48){
-  //     plotArea.clear();
-  //   }
-  //   if (e.keyCode == 49){
-  //     plotArea.checkScroll = 1;
-  //   }
-  //   if (e.keyCode == 50){
-  //     plotArea.checkScroll = 0;
-  //   }
-  //   if (e.keyCode == 189){
-  //       plotArea.scale *=2;
-  //   }
-  //   if (e.keyCode == 187){
-  //     if (Math.round(plotArea.unit*plotArea.scale)>=2){
-  //       plotArea.scale /=2;
-  //     }
-  //   }
-  //   if (e.keyCode == 80){
-  //     plotArea.startTime = new Date();
-  //     for(var i=0;i<globalScope.Output.length;i++){
-  //             globalScope.Output[i].plotValue=[[0,globalScope.Output[i].state]];
-  //       }
-  //     }
-  // });
   document.getElementById("plotArea").addEventListener('mousedown', function(e) {
 
     var rect = plotArea.c.getBoundingClientRect();
