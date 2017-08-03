c7ac1cd49900545566b53434b96c20fa71c41f94
Biswesh Mohapatra <biswesh.mohapatra@iiitb.org>
2017-08-04 03:29:06 +0530

    Adding current page to minimap

--- miniMap.js
+++ miniMap.js
@@ -2,29 +2,41 @@ var miniMapArea = {
 
   canvas: document.getElementById("miniMapArea"),
   setup : function(){
-    var len = simulationArea.maxHeight-simulationArea.minHeight;
-    var wid = simulationArea.maxWidth-simulationArea.minWidth;
-    this.canvas.height = Math.min((len + 80)/2,180);
-    this.canvas.width = Math.min((wid + 80)/2,180);
 
-    if(this.canvas.height == 180 && this.canvas.width == 180){
-      if(len > width)
-        this.canvas.width = (wid/len)*180;
-      else
-        this.canvas.height = (len/wid)*180;
-    }
-    this.canvas.width = Math.max(this.canvas.width,80);
+    this.pageHeight = Math.round(((parseInt($("#simulationArea").height()))/globalScope.scale)/unit)*unit;
+    this.pageWidth = Math.round(((parseInt($("#simulationArea").width()))/globalScope.scale)/unit)*unit;
+    this.pageY = (this.pageHeight-50-globalScope.oy);  // -50 for tool bar? Check again
+    this.pageX =  (this.pageWidth-globalScope.ox);
+
+    this.minY = Math.min(simulationArea.minHeight,this.pageY - this.pageHeight);
+    this.maxY = Math.max(simulationArea.maxHeight ,this.pageY);
+    this.minX = Math.min(simulationArea.minWidth,this.pageX - this.pageWidth);
+    this.maxX = Math.max(simulationArea.maxWidth,this.pageX);
+
+    var len = this.maxY - this.minY;
+    var wid = this.maxX - this.minX;
+
+    this.canvas.height = Math.min(len,250);
+    this.canvas.width = Math.min(wid,250);
     this.canvas.height = Math.max(this.canvas.height,80);
+    this.canvas.width = Math.max(this.canvas.width,80);
+
+    if(len > wid)
+        this.canvas.width = Math.max((wid/len)*this.canvas.height,20);
+    else
+        this.canvas.height = Math.max((len/wid)*this.canvas.width,20);
+
     // console.log("Width"+this.canvas.width);
     // console.log("Height"+this.canvas.height);
     document.getElementById("miniMap").style.height = this.canvas.height  ;
     document.getElementById("miniMap").style.width = this.canvas.width  ;
     this.ctx = this.canvas.getContext("2d");
     $("#miniMap").css("z-index", "1");
-    this.play(len + 10 , wid + 10);
+    this.play(len, wid);
   },
 
   play : function(len, wid){
+
     this.ctx.fillStyle = "MediumVioletRed ";
     this.ctx.rect(0, 0, this.canvas.width, this.canvas.height);
     this.ctx.fill();
@@ -32,10 +44,16 @@ var miniMapArea = {
   },
   resolve : function(lst, length, width){
 
-    var unitHeight = (this.canvas.height-30)/(length);
-    var unitWidth = (this.canvas.width-30)/(width);
-    var unit=Math.min((this.canvas.height-30)/(length),(this.canvas.width-30)/(width));
+    var unitHeight = (this.canvas.height)/(length);
+    var unitWidth = (this.canvas.width)/(width);
+    var unit=Math.max((this.canvas.height)/(length),(this.canvas.width)/(width));
 
+//  to show the area of current canvas
+    this.ctx.beginPath();
+    this.ctx.rect((this.pageX - this.pageWidth - this.minX)*unitWidth, (this.pageY - this.pageHeight - this.minY)*unitHeight, this.pageWidth*unitWidth, this.pageHeight*unitHeight);
+    this.ctx.fillStyle = "pink";
+    this.ctx.fill();
+    this.ctx.stroke();
     for(var i=0;i< lst.length;i++){
         // var unit = unitHeight;
         // if(unitWidth>unitHeight)
@@ -46,9 +64,9 @@ var miniMapArea = {
         {
           this.ctx.beginPath();
           // console.log("wirex"+globalScope[lst[i]][j].node2.absX());
-          this.ctx.moveTo((10+globalScope[lst[i]][j].node1.absX()-simulationArea.minWidth)*unit,(10+globalScope[lst[i]][j].node1.absY()-simulationArea.minHeight+5)*unit);
-          this.ctx.lineTo((10+globalScope[lst[i]][j].node2.absX()-simulationArea.minWidth)*unit,(10+globalScope[lst[i]][j].node2.absY()-simulationArea.minHeight+5)*unit);
-          this.ctx.strokeStyle = "white";
+          this.ctx.moveTo((globalScope[lst[i]][j].node1.absX()-this.minX)*unitWidth,(globalScope[lst[i]][j].node1.absY()-this.minY)*unitHeight);
+          this.ctx.lineTo((globalScope[lst[i]][j].node2.absX()-this.minX)*unitWidth,(globalScope[lst[i]][j].node2.absY()-this.minY)*unitHeight);
+          this.ctx.strokeStyle = "green";
           this.ctx.lineWidth = 1;
           this.ctx.stroke();
         }
@@ -73,7 +91,8 @@ var miniMapArea = {
             // var unit = unitHeight;
             // if(unitWidth<unitHeight)
             //     unit = unitWidth;
-            this.ctx.rect((15+obj.x-obj.leftDimensionX-simulationArea.minWidth)*unit,(15+obj.y-obj.upDimensionY-simulationArea.minHeight)*unit,(obj.rightDimensionX+obj.leftDimensionX)*unit,(obj.downDimensionY+obj.upDimensionY)*unit);
+            var len = Math.max((obj.rightDimensionX+obj.leftDimensionX),(obj.downDimensionY+obj.upDimensionY));
+            this.ctx.rect((5+obj.x-obj.leftDimensionX-this.minX)*unitWidth,(5+obj.y-obj.upDimensionY-this.minY)*unitHeight,len*unit,len*unit);
             // console.log("lx"+obj.leftDimensionX);
             // console.log("rx"+obj.rightDimensionX);
             // console.log("uy"+obj.upDimensionY);
